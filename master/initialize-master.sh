7b24d8b8995e4afe8451f9f2030a8e51b99fda37
Surbhit Awasthi <surbhit.awasthi@go-jek.com>
2020-11-28 11:50:46 +0530

    Refactor cat command input

--- master/initialize-master.sh
+++ master/initialize-master.sh
@@ -6,7 +6,7 @@ echo "host    replication     all             0.0.0.0/0               md5" >> "$
 
 mkdir -p /var/lib/postgresql/archivedir
 
-cat >> ${PGDATA}/postgresql.conf <<EOF
+cat >> "${PGDATA}/postgresql.conf" <<EOF
 wal_level = hot_standby
 archive_mode = on
 archive_command = 'test ! -f /var/lib/postgresql/archivedir/%f && cp %p /var/lib/postgresql/archivedir/%f'
